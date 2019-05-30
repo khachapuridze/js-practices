dec0f548bf9e2b9be3e5957db9b463e77a07a3c3
hacknasa <giorgi.khatchapuridze.1@btu.edu.ge>
2019-05-30 13:52:54 +0400

    feat(03-Functions-Part-1): Finish task-7

---
+++ 03-Functions-Part-1/task-7.js
@@ -0,0 +1,18 @@
+function getDivisors(numb){
+    if (numb === 0){
+        throw new Error ('parameter can\'t be a 0');
+    }
+    if (typeof numb ==='number'){
+        let arr = [];
+        for (i=1; i<=numb; i++){
+            if (numb % i ==0)
+            arr.push(i);
+        }
+        return arr;
+    }
+    else{
+        throw new Error('parameter type is not a Number');
+    }
+}
+
+console.log(getDivisors(12));
