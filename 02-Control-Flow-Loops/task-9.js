d48ca14eb012358137dce582027c79fe5a7e4e67
giorgikhatchapuridze <giorgi.khatchapuridze.1@btu.edu.ge>
2019-05-31 20:47:18 +0400

    feat(02-Control-Flow-Loops): fix var ->let

--- 02-Control-Flow-Loops/task-9.js
+++ 02-Control-Flow-Loops/task-9.js
@@ -2,9 +2,9 @@ let a,b;
 
 for (a = 2 ; a < 10; a++) {
    let condition = true;
-   for (b = 2; b < a; b++) {
+   for (let b = 2; b < a; b++) {
        if(a % b == 0){
-           var condition = false;
+            condition = false;
            break;
        }
    }
