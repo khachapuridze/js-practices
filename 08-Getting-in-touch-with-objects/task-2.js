abe1a4e6f0dbee3cef57875b584469673b215350
giorgikhatchapuridze <giorgi.khatchapuridze.1@btu.edu.ge>
2019-06-05 20:59:12 +0400

    feat(08-Getting-in-touch-with-objects): fix task-2

--- 08-Getting-in-touch-with-objects/task-2.js
+++ 08-Getting-in-touch-with-objects/task-2.js
@@ -1,22 +1,22 @@
-// # Task 2
+// // # Task 2
 
-// Create object `person` with two properties: `rate` and `salary`.
+// // Create object `person` with two properties: `rate` and `salary`.
 
-// The property `rate` can be read and written, but not deleted, 
-// and this property should not participate in the enumeration of all properties when iterating.
+// // The property `rate` can be read and written, but not deleted, 
+// // and this property should not participate in the enumeration of all properties when iterating.
 
-// Property `salary` can be read, but can not be changed. When reading the property, 
-// `salary` returns the result of multiplying the ` rate` field by the current day in the month.
-//  If the rate is not set - return the number 0.
+// // Property `salary` can be read, but can not be changed. When reading the property, 
+// // `salary` returns the result of multiplying the ` rate` field by the current day in the month.
+// //  If the rate is not set - return the number 0.
 
-// Example:
+// // Example:
 
-// ```javascript
-// person.rate = 30;
+// // ```javascript
+// // person.rate = 30;
 
-// // Suppose that today is January 10th, in this case this property returns the number 300
-// person.salary;
-// ```
+// // // Suppose that today is January 10th, in this case this property returns the number 300
+// // person.salary;
+// // ```
 
 
 const person = {
@@ -28,6 +28,9 @@ const person = {
         const today = new Date();
         const day = today.getDate();
         return day * this.rate;
+    },   
+    set salary(any){
+        throw new Error ('not seted');
     }
 };
 
