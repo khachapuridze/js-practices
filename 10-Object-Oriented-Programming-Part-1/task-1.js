53c3a6bcfe05f9d85548f49c46dee25733487bd5
giorgikhatchapuridze <giorgi.khatchapuridze.1@btu.edu.ge>
2019-06-05 21:46:41 +0400

    feat(10-Object-Oriented-Programming-Part-1): finish task 1-2

---
+++ 10-Object-Oriented-Programming-Part-1/task-1.js
@@ -0,0 +1,26 @@
+function CoffeeMachine(power) {
+	this.waterAmount = 0;
+	let WATER_HEAT_CAPACITY = 4200;
+	let self = this;
+	
+    function getBoilTime() {
+		return self.waterAmount * WATER_HEAT_CAPACITY * 80 / power;
+	}
+	
+    function onReady() {
+		return 'Coffee is ready';
+	}
+
+    this.run = function() {
+		timerId =  setTimeout(onReady, getBoilTime());
+    };
+    this.stop = function(){
+        clearTimeout(timerId);
+        throw new Error("Coffee isn't ready");
+    }
+}
+
+let coffeeMachine = new CoffeeMachine(50000);
+coffeeMachine.waterAmount = 200;
+coffeeMachine.run();
+coffeeMachine.stop();
---
+++ 10-Object-Oriented-Programming-Part-1/task-2.js
@@ -0,0 +1,25 @@
+function CoffeeMachine(power, capacity) {
+ 
+     this.setWaterAmount = function(amount) {
+         if (amount < 0) {
+             throw new Error("Value has to be positive.");
+         }
+         if (amount > capacity) {
+             throw new Error("You can't put more water, than " + capacity);
+         }
+         waterAmount = amount;
+     };
+ 
+     this.getWaterAmount = function() {
+         return waterAmount;
+    };
+
+    const coffePower = power;
+    this.getter = function() {
+        return coffePower;
+    }
+ }
+ 
+ let coffeeMachine = new CoffeeMachine(100,100);
+coffeeMachine.setWaterAmount = 150;
+ console.log(coffeeMachine.getter());
