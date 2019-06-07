ee691f109d097944a82407e77ba4073b3c91cd46
giorgikhatchapuridze <giorgi.khatchapuridze.1@btu.edu.ge>
2019-06-07 21:12:49 +0400

    feat(11-Object-Oriented-Programming-Part-2): fix task-2

--- 11-Object-Oriented-Programming-Part-2/task-2.js
+++ 11-Object-Oriented-Programming-Part-2/task-2.js
@@ -1,19 +1,22 @@
 function CoffeeMachine(power) {
+	this.power = power;
 	this.waterAmount = 0;
 	this.WATER_HEAT_CAPACITY = 4200;
+}
 	
-    this.getTimeToBoil = function() {
-		return this.waterAmount * this.WATER_HEAT_CAPACITY * 80 / power;
+	CoffeeMachine.prototype.getTimeToBoil = function() {
+		return this.waterAmount * this.WATER_HEAT_CAPACITY * 80 / this.power;
 	}	
     CoffeeMachine.prototype.run = function() {
     	setTimeout(function() {
 			console.log('Coffee is ready!');
     	}, this.getTimeToBoil());
 	};
+
 	CoffeeMachine.prototype.setWaterAmount = function(amount) {
 		this.waterAmount = amount;
 	};
-}
+
 
 
 let coffeeMachine = new CoffeeMachine(10000);
