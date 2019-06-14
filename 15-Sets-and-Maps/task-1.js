b95949ae043702dbbe1fa2a3de71a695d92503f1
giorgi khatchapuridze <32338447+hacknasa@users.noreply.github.com>
2019-06-14 11:51:24 +0400

    feat(15-Sets-and-Maps): Finish task-1

--- 15-Sets-and-Maps/task-1.js
+++ 15-Sets-and-Maps/task-1.js
@@ -1,50 +1,84 @@
 class DB {
-
-    constructor(){
-        
+    constructor() {
+        this.database = new Map();
     }
 
     create(obj) {
-        obj = new Map();
-        
-        var timestamp = new Date().getUTCMilliseconds();
-        if(typeof obj === 'object'){
-            obj.set('id',timestamp);
-            return obj;
+      let id = new Date().getUTCMilliseconds().toString();
+        if (typeof obj === 'object' || obj) {
+            if (typeof obj.name === 'string' && typeof obj.age === 'number' &&  typeof obj.country === 'string' &&  typeof obj.salary === 'number') {
+                this.database.set(id, obj);
+                return id;
+            }
+            else {
+                throw new Error('Wrong properties');
+            }
         }
-        else{
-            throw new Error("It's not an object");
-        }
-        
+            else {
+                 throw new Error("It's not an object");
+            }
     }
 
-    read(id){
-        let user = new Map();
-        if(!id){
-            throw new Error('Empty');
-        }
-        // if(typeof id !== 'string'){
-        //     throw new Error ("Not a string");
-        // }
-        if(user.get(id) == id){
-            return true;
+    read(id) {
+        if (id || typeof id === 'string') {
+            if (this.database.has(id)) {
+                return this.database.get(id).id=id;
+            } 
+            else {
+                return null;
+            }
         }
         else {
-            return null;
+            throw new Error('id is non-string');
         }
     }
 
-    readAll(arg){
-
-        if(arg){
-            throw new Error('no need arguments');
-
+    readAll(arg) {
+        if (arg) {
+            throw new Error('a parameter is passed to the readAll method');
         }
-        else{
-            // return obj;
+        else {
+            return Array.from(this.database.values());
+        }
+    }
+
+    update(id, object) {
+        if (!id || typeof id !== 'string') {
+            throw new Error('ID must be excisting string');
+        }
+
+        if (!object || typeof object !== 'object') {
+            throw new Error('Not object');
+        }
+
+        if (this.database.has(id)) {
+            let res = this.database.get(id);
+            this.database.set(id, { ...res, ...object });
+            return id;
+        } 
+        else {
+            throw new Error('There is no user');
+        }
+    }
+
+    delete(id) {
+        if (id || typeof id === 'string') {
+            if (this.database.has(id)) {
+                this.database.delete(id);
+                return true;
+            } 
+            else {
+                throw new Error('There is no user');
+            }
+        } 
+        else {
+            throw new Error('id is not string');
         }
     }
 }
+
+
+
 
 const person = {
     name: 'Pitter', // required field with type string
@@ -59,3 +93,6 @@ console.log(id);
 const customer = db.read(id);
 console.log(customer);
 const customers = db.readAll();
+db.update(id, { age: 22 }); // id
+db.delete(id); // true
+
