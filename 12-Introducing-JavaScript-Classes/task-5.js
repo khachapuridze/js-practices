6409900026220eb6e697f255d5e05a60c81ec2ae
giorgi khatchapuridze <32338447+hacknasa@users.noreply.github.com>
2019-06-14 10:10:19 +0400

    feat(12-Introducing-JavaScript-Classes): Finish task-5

--- 12-Introducing-JavaScript-Classes/task-5.js
+++ 12-Introducing-JavaScript-Classes/task-5.js
@@ -3,39 +3,23 @@ const Validator = class{
 
     }
   
-    isEmail(email){
-      let valid= /\S+@\S+\.\S+/;
-      if(valid.test(email)){
-        return true;
-      }
-      else {
-        return false;
-      }
-  
+    isEmail(email) {
+        var res = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return res.test(String(email).toLowerCase());
     }
-
-    isDomain(domain){
-        let res = domain.split('');
-        if(res.includes('.')){
-            return true;
-        }
-        else{
-            return false;
-        }
-
+    isDomain(domain) {
+        var res = /[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+/;
+        return res.test(String(domain).toLowerCase());
     }
-
-
-  
-    isPhone(phone){
-      let res = phone.split('');
-      if(res[0] == '995' && res[1].includes('(')){
-        return true;
-      }
-      else {
-        return false;
-      }
+    isDate(date) {
+        var res = /([0-9]{2}[\./]){2}[0-9]{4}/;
+        return res.test(date);
     }
+    isPhone(phone) {
+        let re = /^\+([0-9]+)+\s+\([0-9]+\)+\s+([0-9]+-[0-9]+-[0-9])+[0-9]/;//TODO: fix this case
+        return re.test(String(phone).toLowerCase());
+    }
+}
   
   }
   
@@ -45,5 +29,5 @@ const Validator = class{
   
   console.log(validator.isEmail('jshtml@mail.ru'));
   console.log(validator.isDomain('jshtml.net'));
-  // console.log(validator.isDate('12.05.2020'));
+  console.log(validator.isDate('12.05.2020'));
   console.log(validator.isPhone('+995 (29) 817-68-92'));
